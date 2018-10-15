520aa7d24ba4bc20d9f48494bc38a87244c52a94
kiyohiro kawamura <kyorohiro@gmail.com>
2018-10-16 05:41:11 +0900

    fc

---
+++ README.md
@@ -0,0 +1,8 @@
+# domsprite
+
+
+# license
+
+Apache-2.0
+
+https://opensource.org/licenses/Apache-2.0
---
+++ example/dart_example.dart
@@ -0,0 +1,6 @@
+//import 'package:dart/dart.dart';
+
+main() {
+  //var awesome = new Awesome();
+  print('awesome');
+}
